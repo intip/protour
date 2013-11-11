bca5096b57e7580fc5a44c3d68accaea5a8579c1
Guilherme Vierno <guilhermevierno@gmail.com>
2013-11-11 17:57:48 -0200

    Adiciona resumo do pacote.

--- project/test_settings.py
+++ project/test_settings.py
@@ -12,6 +12,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'test_portalturismo',
+        'TEST_NAME': 'test_portalturismo',
         'USER': 'postgres',
         'PASSWORD': '1234',
         'HOST': 'localhost',
