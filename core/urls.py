64c40b0e6d5ed180a5aad2953eb98f5ebce58048
Guilherme Vierno <guilhermevierno@gmail.com>
2013-11-12 17:48:09 -0200

    Adiciona os novos campos textuais do pacote.

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
 
-from pacote.models import Pacote
+from pacote.models import Pacote, Variacao
 
 
 from core.views import HomePageView
@@ -13,7 +13,8 @@ from core.views import HomePageView
 urlpatterns = patterns(
     'core.views',
     url(r'^$', HomePageView.as_view(), name='homepage'),
-    url(r'^comprar/$', TemplateView.as_view(template_name="comprar.html")),
+    url(r'^comprar/(?P<pk>\d+)/$',
+        TemplateView.as_view(template_name="comprar.html")),
     url(r'^pacote/(?P<pk>\d+)/$', DetailView.as_view(
         model=Pacote), name='detalhes'),
 )
