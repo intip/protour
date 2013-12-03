6a34abe08fb1f4f6dd4b5a12ba5654395a502020
Guilherme Vierno <guilhermevierno@gmail.com>
2013-12-03 16:41:43 -0200

    Altera coisa demais para caber em uma mensagem.

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@
 from django.conf.urls import patterns, url
 from django.views.generic.detail import DetailView
 
-from pacote.models import Pacote
+from pacote.models import Pacote, Variacao
 
 from core.views import HomePageView, DestinoView, TypeAheadView
 
@@ -12,7 +12,7 @@ urlpatterns = patterns(
     'core.views',
     url(r'^$', HomePageView.as_view(), name='homepage'),
     url(r'^comprar/(?P<pk>\d+)/$',
-        DetailView.as_view(model=Pacote, template_name="comprar.html"),
+        DetailView.as_view(model=Variacao, template_name="comprar.html"),
         name='comprar'),
     url(r'^pacote/(?P<slug>[\w_-]+)/$', DetailView.as_view(
         model=Pacote), name='detalhes'),
