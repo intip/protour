7725c87f7d64e6a97d53ec9919b2a55c41f8feae
Diêgo Bolina <diego.bolina@gmail.com>
2013-12-05 16:53:54 -0200

    Ajusta scroll follow da sidebar

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -1,1 +1,35 @@
+var offset = $(".well.comprar").offset();
+var topPadding = 0;
 
+var heightColuna = $(".detalhes-pacote .col-md-9").height();
+
+function heightSidebar(){
+    $('.col-md-3.sidebar').css('height',heightColuna)
+}
+
+$(window).scroll(function() {
+
+    heightSidebar();
+
+    if ($(window).scrollTop() > offset.top) {
+        $(".well.comprar").stop().animate({
+            marginTop: $(window).scrollTop() - offset.top + 10,
+        });
+
+        if($(window).scrollTop() > heightColuna + 250 ){
+            $(".well.comprar").stop().animate({
+                marginTop: $(window).scrollTop() - offset.top - 120 ,
+            });
+        }
+    } 
+  
+    else {
+        $(".well.comprar").stop().animate({
+            marginTop: 0
+        });
+    };
+
+    
+
+
+});
