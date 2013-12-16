16a7010aa5d533892910d65f880bec997460f0d4
Guilherme Vierno <guilhermevierno@gmail.com>
2013-12-16 13:45:31 -0200

    Corrige menu e javascript de animação.

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -1,21 +1,22 @@
 var offset = $(".well.comprar").offset();
 var topPadding = 0;
 
+if ($(".well.comprar").length > 0) {
+    $(window).scroll(function() {
+        var heightColuna = $(".detalhes-pacote").height();
+        var heightWell = $(".sidebar .comprar").height() + 70;
+        var max_height = offset.top + heightColuna - heightWell;
 
-$(window).scroll(function() {
-    var heightColuna = $(".detalhes-pacote").height();
-    var heightWell = $(".sidebar .comprar").height() + 70;
-    var max_height = offset.top + heightColuna - heightWell;
+        if ($(window).scrollTop() > offset.top && $(window).scrollTop() < max_height) {
+            $(".well.comprar").stop().animate({
+                marginTop: $(window).scrollTop() - offset.top + 10,
+            });
+        }
 
-    if ($(window).scrollTop() > offset.top && $(window).scrollTop() < max_height) {
-        $(".well.comprar").stop().animate({
-            marginTop: $(window).scrollTop() - offset.top + 10,
-        });
-    }
-
-    else if ($(window).scrollTop() < max_height) {
-        $(".well.comprar").stop().animate({
-            marginTop: 0
-        });
-    }
-});
+        else if ($(window).scrollTop() < max_height) {
+            $(".well.comprar").stop().animate({
+                marginTop: 0
+            });
+        }
+    });
+}
